d1c428142230e92d9c1fc91f5cb3c70dee656dc4
Kenn-C <cuetokenneth31@gmail.com>
2025-01-13 17:14:20 +0800

    Initial commit

--- script.js
+++ script.js
@@ -131,3 +131,24 @@ document.addEventListener('DOMContentLoaded', () => {
    
     startBarUpdates();
 });
+
+
+const modal = document.getElementById("portfolio-modal");
+const openModalBtn = document.getElementById("open-modal");
+const closeModalBtn = document.getElementById("close-modal");
+
+openModalBtn.addEventListener("click", () => {
+modal.classList.remove("hidden");
+});
+
+closeModalBtn.addEventListener("click", () => {
+modal.classList.add("hidden");
+});
+
+window.addEventListener("click", (event) => {
+if (event.target === modal) {
+    modal.classList.add("hidden");
+}
+});
+
+
